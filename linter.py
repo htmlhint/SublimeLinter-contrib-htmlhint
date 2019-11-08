b1ed94e53ae22a51f082733e22315561888956d7
herr kaste <herr.kaste@gmail.com>
2019-11-08 18:46:56 +0100

    Remove unused flags
    - `version_*` are unused
    - `config-*` are unused
    - `regex` should not be changed for JSON reporters

--- linter.py
+++ linter.py
@@ -18,16 +18,10 @@ class Htmlhint(NodeLinter):
 
     """Provides an interface to htmlhint."""
 
+    cmd = ('htmlhint', '--format', 'json', '--nocolor', 'stdin')
     defaults = {
         'selector': 'text.html'
     }
-    cmd = ('htmlhint', '--format', 'json', '--nocolor', 'stdin')
-    version_args = '--version'
-    version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 0.9.13'
-    # empty regex so plugin initializes properly
-    regex = r''
-    config_file = ('--config', '.htmlhintrc')
 
     def find_errors(self, output):
         """
